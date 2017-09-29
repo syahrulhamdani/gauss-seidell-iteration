98f978b340e448222ac128340be418f457ee77df
hamdani <hamdani.inbox@gmail.com>
2017-09-29 13:03:38 +0700

    Initial commit strict-diagonal.cpp

---
+++ strictly-diagonally-dominant-matrix/strict-diagonal.cpp
@@ -0,0 +1,18 @@
+// Program to determine whether a matrix's
+// included in stricly diagonal dominant matrix
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+void matrix_input();
+
+int main () {
+
+  // Variable Declaration
+
+  // Main Program
+
+  return 0;
+}
