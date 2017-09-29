55c4e7e3f47c72dbde84cd99f188611ca6ef3306
hamdani <hamdani.inbox@gmail.com>
2017-09-29 16:28:26 +0700

    Add matrix input process through external files

--- gauss-seidel/gauss-seidel.cpp
+++ gauss-seidel/gauss-seidel.cpp
@@ -15,7 +15,20 @@ void matrix_analysis (size);
 void matrix_analysis () {
 
   // Local Variable Declaration
+  ifstream matrix;
+  int i, j;
 
+  // Input matrix from external .txt files
+  matrix.open("matrix-input.txt");
+  if (!matrix) {
+    cout << "Warning! Please check your file name or the directory" << endl;
+  }
+
+  for (i = 0; i < size; i++) {
+    for (j = 0; j < size; j++) {
+      matrix >> my_matrix[i][j];
+    }
+  }
   // ---------------------- END OF MATRIX INPUT PROCESS ----------------------
 
   // Main Program
