d4ee4138733305c35b748ac1de66c4379b2910da
hamdani <hamdani.inbox@gmail.com>
2017-09-29 13:00:36 +0700

    Inital commit jacobi.cpp

---
+++ jacobi/jacobi.cpp
@@ -0,0 +1,16 @@
+// Jacobi Iterative Method
+// to determine solution of linear equation system
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main () {
+
+  // Variable Declaration
+
+  // Main Program
+
+  return 0
+}
